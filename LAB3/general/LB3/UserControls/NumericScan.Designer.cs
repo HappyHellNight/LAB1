1bcf4c484bfb146b88848b7d2549d41f4ed908aa
MakyHaky <A1921sk@gmail.com>
2021-04-12 22:28:59 +0300

    p3

---
+++ LAB3/general/LB3/UserControls/NumericScan.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace LB3.StudName
+{
+    partial class NumericScan
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLeft = new LB3.StudName.ArrowButton(this.components);
+            this.btnRight = new LB3.StudName.ArrowButton(this.components);
+            this.txtBox = new System.Windows.Forms.TextBox();
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(0, 0);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.ScrollButton = System.Windows.Forms.ScrollButton.Left;
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 0;
+            this.btnLeft.Text = "arrowButton1";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.ButtonOnClick);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(0, 0);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.ScrollButton = System.Windows.Forms.ScrollButton.Right;
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 0;
+            this.btnRight.Text = "arrowButton2";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.ButtonOnClick);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(0, 0);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(100, 20);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtBox.TextChanged += new System.EventHandler(this.TextBoxOnTextChanged);
+            this.txtBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxOnKeyDown);
+
+        }
+
+        #endregion
+
+        private ArrowButton btnLeft;
+        private ArrowButton btnRight;
+        private System.Windows.Forms.TextBox txtBox;
+    }
+}
---
+++ LAB3/general/LB3/UserControls/NumericScan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB3.StudName
+{
+    public partial class NumericScan : Component
+    {
+        public NumericScan()
+        {
+            InitializeComponent();
+        }
+
+        public NumericScan(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        private void TextBoxOnTextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxOnKeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+
+        }
+
+        private void ButtonOnClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
